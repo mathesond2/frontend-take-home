4ad5a2a999b95dc6d3cdd2c1bd693e7bc773d601
mathesond2 <mathesond2@gmail.com>
2023-05-01 09:27:38 -0400

    add Chakra-ui

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import './App.css';
 import NpmList from './components/NpmList';
 import Search from './components/Search';
 import { useNpmData } from './hooks/useNpmData';
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
   const { npmData, fetchNpmData } = useNpmData();
   return (
-    <>
+    <ChakraProvider>
       <Search onSearch={fetchNpmData} />
       <NpmList npmData={npmData} />
-    </>
+    </ChakraProvider>
   );
 }
 
