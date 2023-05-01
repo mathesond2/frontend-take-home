7a84128b0ae025c354cfc03bf2813f761ccb1a6a
mathesond2 <mathesond2@gmail.com>
2023-05-01 12:00:33 -0400

    add err boundary

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
+import { ErrorBoundary } from 'react-error-boundary';
 import Layout from './components/Layout';
 import NpmList from './components/NpmList/NpmList';
 import Search from './components/Search/Search';
 import { useNpmData } from './hooks/useNpmData';
 import { ChakraProvider } from '@chakra-ui/react'
+import ErrorBoundaryFallback from './components/ErrorBoundaryFallback';
 
 function App() {
   const { npmData, fetchNpmData } = useNpmData();
   return (
     <ChakraProvider>
       <Layout>
-        <Search onSearch={fetchNpmData} />
-        <NpmList npmData={npmData} />
+        <ErrorBoundary FallbackComponent={ErrorBoundaryFallback}>
+          <Search onSearch={fetchNpmData} />
+          <NpmList npmData={npmData} />
+        </ErrorBoundary>
       </Layout>
     </ChakraProvider>
   );
