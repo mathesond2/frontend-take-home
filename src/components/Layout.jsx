84b741566c4dda653a71f961bb409884a39c46d3
mathesond2 <mathesond2@gmail.com>
2023-05-01 10:15:57 -0400

    add Navbar

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,10 @@
 import { Box, Container } from '@chakra-ui/react';
+import Navbar from './Navbar/Navbar';
 
 export default function Layout({ children }) {
   return (
     <Container maxW="full" as="main" paddingX={[6, 6, 10]}>
+      <Navbar />
       <Box pt={4}>{children}</Box>
     </Container>
   );
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+import { Heading } from "@chakra-ui/layout";
+
+export default function Navbar() {
+  return (
+      <header>
+        <Heading as="h1" size="lg">NPM Package Search</Heading>
+      </header>
+  )
+}
