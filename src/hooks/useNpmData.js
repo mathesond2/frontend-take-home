6896452f92625d8b32a0d7a59ef1fa17b67aa428
mathesond2 <mathesond2@gmail.com>
2023-05-01 08:59:52 -0400

    add custom hook, reducer

---
+++ src/hooks/useNpmData.js
@@ -0,0 +1,35 @@
+import { useReducer } from 'react';
+import npmDataReducer from '../reducers/npmDataReducer';
+
+function buildUrl(query) {
+  return `https://api.npms.io/v2/search/suggestions?q=${query}`;
+}
+
+const initialData = {
+  data: [],
+  loading: false,
+  error: null,
+};
+
+export function useNpmData() {
+  const [npmData, dispatchNpmData] = useReducer(npmDataReducer, initialData);
+
+  async function fetchNpmData(val) {
+    try {
+      dispatchNpmData({ type: 'fetch' });
+      if (val.length === 0) {
+        dispatchNpmData({ type: 'success', data: [] });
+        return;
+      }
+
+      const data = await fetch(buildUrl(val));
+      const parsedData = await data.json();
+      dispatchNpmData({ type: 'success', data: parsedData });
+    } catch (error) {
+      dispatchNpmData({ type: 'error', error });
+      console.error('error: ', error);
+    }
+  }
+
+  return { npmData, fetchNpmData };
+}
