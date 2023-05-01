bc2916ea3acfd03a22c28343f4ebdbcd7ad36b96
mathesond2 <mathesond2@gmail.com>
2023-05-01 12:30:03 -0400

    sort imports

--- src/components/Search/Search.test.js
+++ src/components/Search/Search.test.js
@@ -1,5 +1,5 @@
-import userEvent from '@testing-library/user-event';
 import { fireEvent, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Search from './Search';
 
 describe('Search', () => {
